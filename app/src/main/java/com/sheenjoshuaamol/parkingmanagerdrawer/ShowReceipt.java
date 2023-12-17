4c43e983b867ff27518b6081833a9927b53f9de0
SheenAmol141 <alpyne141@gmail.com>
2023-12-18 01:16:23 +0800

    finished build with payed receipt upload to firestore

--- app/src/main/java/com/sheenjoshuaamol/parkingmanagerdrawer/ShowReceipt.java
+++ app/src/main/java/com/sheenjoshuaamol/parkingmanagerdrawer/ShowReceipt.java
@@ -1,20 +1,30 @@
 package com.sheenjoshuaamol.parkingmanagerdrawer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ShowReceipt extends AppCompatActivity {
 
+
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +44,6 @@ public class ShowReceipt extends AppCompatActivity {
         totalTime = findViewById(R.id.tvTotalTime);
         totalPrice = findViewById(R.id.tvTotalPrice);
 
-        Date exited = new Date();
 
         String formatPattern = "EEE MMM dd HH:mm:ss z yyyy";
         SimpleDateFormat format = new SimpleDateFormat(formatPattern);
@@ -43,20 +52,9 @@ public class ShowReceipt extends AppCompatActivity {
         try {
             entered = format.parse(map.get("timeEntered"));
 
+            Long totalhours = printDifference(entered, new Date());
 
-
-
-
-
-
-
-
-
-
-
-
-
-            totalTime.setText("" + entered);
+            totalTime.setText("Total Hours: " + totalhours );
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
@@ -68,6 +66,92 @@ public class ShowReceipt extends AppCompatActivity {
         timeEntered.setText("Time Entered: " + " " +  map.get("timeEntered"));
         timeExited.setText("Time Exited: " + " " + new Date());
 
+        String price;
 
+        if (printDifference(entered, new Date()) >= 2){
+            int pay;
+            Long c = printDifference(entered, new Date());
+                c = c - 3;
+             pay = 30;
+                while (c > 0) {
+                    c = c - 1;
+                     pay = pay + 5;
+                }
+                price = "₱" + pay;
+        } else {
+            price = "₱30";
+        }
+
+        totalPrice.setText("Total Payment: "+ price+".00");
+
+
+        Button payed = findViewById(R.id.payed);
+        payed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Map<String, Object> record = new HashMap<>();
+
+
+                record.put("Name", map.get("name"));
+                record.put("Parking Spot", map.get("code"));
+                record.put("Plate Number", map.get("plate"));
+                record.put("Time Entered", map.get("timeEntered"));
+                record.put("Time Exited", format.format(new Date()));
+                record.put("Total Hours", totalTime.getText().toString() + " Hours");
+                record.put("Total Payment",  price+".00");
+                db.collection("RECEIPTS PAYED").add(record).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(ShowReceipt.this, "Receipt Uploaded! Thank you!", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ShowReceipt.this, "Receipt Upload Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+
+            }
+        });
+
+
+
+    }
+    //1 minute = 60 seconds
+//1 hour = 60 x 60 = 3600
+//1 day = 3600 x 24 = 86400
+    public long printDifference(Date startDate, Date endDate){
+
+        //milliseconds
+        long different = endDate.getTime() - startDate.getTime();
+
+        System.out.println("startDate : " + startDate);
+        System.out.println("endDate : "+ endDate);
+        System.out.println("different : " + different);
+
+        long secondsInMilli = 1000;
+        long minutesInMilli = secondsInMilli * 60;
+        long hoursInMilli = minutesInMilli * 60;
+        long daysInMilli = hoursInMilli * 24;
+
+        //long elapsedDays = different / daysInMilli;
+        //different = different % daysInMilli;
+
+        long elapsedHours = different / hoursInMilli;
+        different = different % hoursInMilli;
+
+        long elapsedMinutes = different / minutesInMilli;
+        different = different % minutesInMilli;
+
+        long elapsedSeconds = different / secondsInMilli;
+
+        System.out.printf(
+                "%d hours, %d minutes, %d seconds%n",
+                elapsedHours, elapsedMinutes, elapsedSeconds);
+        return elapsedHours;
     }
 }
